91374ff9f2c2bc4df73c8926c82aa2c7d0337262
Gabriel Valin <gabrielvalincontato@gmail.com>
2022-04-16 03:22:19 -0300

    simplify code with beforeEach and remove sut factory method

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -3,32 +3,26 @@ import { FacebookAuthenticationService } from "@/data/services/facebook-authenti
 import { AuthenticationError } from "@/domain/errors"
 import { mock, MockProxy } from "jest-mock-extended"
 
-type SutTypes = {
-  sut: FacebookAuthenticationService
-  loadFacebookUserApi: MockProxy<LoadFacebookUser>
-}
-const makeSut = (): SutTypes => {
-  const loadFacebookUserApi = mock<LoadFacebookUser>()
-  const sut = new FacebookAuthenticationService(loadFacebookUserApi)
-  return {
-    sut,
-    loadFacebookUserApi
-  }
-}
-
 describe('FacebookAuthenticationService', () => {
-  it("should call facebook api called with correct param", async () => {
-    const { sut, loadFacebookUserApi} = makeSut()
-    await sut.perform({ token: 'any_token' })
+  let sut: FacebookAuthenticationService
+  let loadFacebookUserApi: MockProxy<LoadFacebookUser>
+  const mockToken = { token: 'any_token' }
 
-    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
+  beforeEach(() => {
+    loadFacebookUserApi = mock()
+    sut = new FacebookAuthenticationService(loadFacebookUserApi)
+  })
+
+  it("should call facebook api called with correct param", async () => {
+    await sut.perform(mockToken)
+
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith(mockToken)
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
-    const { sut, loadFacebookUserApi} = makeSut()
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
-    const authResult = await sut.perform({ token: 'any_token' })
+    const authResult = await sut.perform(mockToken)
 
     expect(authResult).toEqual(new AuthenticationError())
   })
