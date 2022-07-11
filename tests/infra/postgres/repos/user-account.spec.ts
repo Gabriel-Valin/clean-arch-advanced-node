a0dca2b8d00588b069a6c4a6dd215dea734d9e64
Gabriel Valin <gabrielvalincontato@gmail.com>
2022-07-11 11:17:03 -0300

    test: refactor test user account and explain how to work db connection

---
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -0,0 +1,45 @@
+import { PgUserAccountRepository } from '@/infra/postgres/repos'
+import { PgUser } from '@/infra/postgres/entities'
+import { makeFakeDb } from '@/tests/infra/postgres/mocks'
+
+import { IBackup } from 'pg-mem'
+import { getRepository, Repository, getConnection } from 'typeorm'
+
+describe('PgUserAccountRepository', () => {
+  describe('load', () => {
+    let sut: PgUserAccountRepository
+    let pgUserRepo: Repository<PgUser>
+    let backup: IBackup
+
+    beforeAll(async () => {
+      const db = await makeFakeDb([PgUser])
+      backup = db.backup()
+      pgUserRepo = getRepository(PgUser)
+    })
+
+    afterAll(async () => {
+      await getConnection().close()
+    })
+
+    beforeEach(() => {
+      backup.restore()
+      sut = new PgUserAccountRepository()
+    })
+
+    it('should return an account if email exists', async () => {
+      await pgUserRepo.save({ email: 'any_email' })
+
+      const account = await sut.load({ email: 'any_email' })
+
+      expect(account).toEqual({ id: '1' })
+    })
+
+    it('should return undefined if email does not exists', async () => {
+      const sut = new PgUserAccountRepository()
+
+      const account = await sut.load({ email: 'any_email' })
+
+      expect(account).toBeUndefined()
+    })
+  })
+})
